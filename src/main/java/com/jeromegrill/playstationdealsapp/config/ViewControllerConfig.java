c4fdaafcb964c3cb73ba9d3e099db87597b872ae
JeRomeGrill <glushko.r@gmail.com>
2023-01-10 10:41:34 +0300

    Project started

--- src/main/java/com/jeromegrill/playstationdealsapp/config/ViewControllerConfig.java
+++ src/main/java/com/jeromegrill/playstationdealsapp/config/ViewControllerConfig.java
@@ -1,2 +1,14 @@
-package com.jeromegrill.playstationdealsapp.config;public class ViewControllerConfig {
+package com.jeromegrill.playstationdealsapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ViewControllerConfig implements WebMvcConfigurer {
+    public void addViewControllers(ViewControllerRegistry registry) {
+//        registry.addViewController("/login").setViewName("login");
+//        registry.addViewController("/").setViewName("main");
+
+    }
 }
--- src/test/java/com/jeromegrill/playstationdealsapp/PlaystationDealsAppApplicationTests.java
+++ src/test/java/com/jeromegrill/playstationdealsapp/PlaystationDealsAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlaystationDealsAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
